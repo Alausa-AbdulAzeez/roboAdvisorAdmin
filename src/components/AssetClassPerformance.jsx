582d79081ba412acb8278227ed0a4fe41fe11fd1
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2024-03-30 16:36:00 +0100

    changed logo

--- src/components/AssetClassPerformance.jsx
+++ src/components/AssetClassPerformance.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
+import AssetClassPerformanceTable from "./AssetClassPerformanceTable";
 
 const AssetClassPerformance = () => {
   return (
-    <div className="w-[62%] flex flex-col gap-[30px] bg-white border border-borderColor rounded-[8px] p-[24px] h-[443px]"></div>
+    <div className="w-[62%] flex flex-col gap-[24px] bg-white border border-borderColor rounded-[8px] p-[24px] h-[443px]">
+      <div className="uppercase text-[14px] font-[400] leading-[28px] text-blackTextColor">
+        asset class performance
+      </div>
+      <AssetClassPerformanceTable />
+    </div>
   );
 };
 
---
+++ src/components/AssetClassPerformanceTable.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AssetClassPerformanceTable = () => {
+  return <div className="flex-1">AssetClassPerformanceTable</div>;
+};
+
+export default AssetClassPerformanceTable;
