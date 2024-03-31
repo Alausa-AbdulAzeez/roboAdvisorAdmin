561d832af62e3916f73d831510f222f64b38c21f
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2024-03-31 11:09:55 +0100

    made sidebar responsive

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { Icon } from "@iconify/react";
 import {
   activityLogInactive,
   arrowDown,
@@ -14,10 +15,14 @@ import {
 } from "../assets/icons";
 
 const ActiveSidebarBorder = () => {
-  return <div className="h-[36px] w-[9px] bg-mainBlue rounded-[56px]"></div>;
+  return (
+    <div className="h-[36px] max-2xl:h-[28.8px]  w-[9px] max-2xl:w-[7.2px] bg-mainBlue rounded-[56px]"></div>
+  );
 };
 const InactiveSidebarBorder = () => {
-  return <div className="h-[36px] w-[9px] bg-none rounded-[56px]"></div>;
+  return (
+    <div className="h-[36px] max-2xl:h-[28.8px] w-[9px]  max-2xl:w-[7.2px] bg-none rounded-[56px]"></div>
+  );
 };
 
 const Sidebar = () => {
@@ -27,51 +32,61 @@ const Sidebar = () => {
       title: "Dashboard",
       activeImage: dashboarrdActive,
       inActiveImage: dashboardInactive,
+      iconName: "mage:dashboard-chart-fill",
       url: "/",
     },
     {
       title: "Users",
       activeImage: usersActive,
       inActiveImage: usersInactive,
+      iconName: "heroicons:user-20-solid",
       url: "/users",
     },
     {
       title: "Portfolios",
       activeImage: portfoliosActive,
       inActiveImage: portfoliosInactive,
+      iconName: "dashicons:portfolio",
       url: "/portfolios",
     },
     {
       title: "Transactions",
       activeImage: transactionsActive,
       inActiveImage: transactionsInactive,
+      iconName: "icon-park-solid:transaction",
       url: "/transactions",
     },
   ];
 
   return (
-    <div className="zoom h-full w-[271px] bg-white border-r border-borderColor">
-      <div className="mx-auto w-[207px] h-[64px] flex items-center text-left text-mainBlue text-[24px] font-[900] leading-[28px]">
+    <div className="h-full max-2xl:w-[216.8px] w-[271px] bg-white border-r border-borderColor">
+      <div className="mx-auto w-[207px] max-2xl:w-[165.6px] h-[64px] max-2xl:h-[51.8px] flex items-center text-left text-mainBlue text-[24px] max-2xl:text-[19.2px font-[900] leading-[28px] max-2xl:leading-[22.4px] tracking-[-0.41px] max-2xl:tracking-[-0.33px]">
         <Link to={"/"}>Robovisor</Link>
       </div>
-      <div className="mt-[16px] w-full">
+      <div className="mt-[16px] max-2xl:mt-[12.8px] w-full">
         {sidebarData?.map((singleSidebarData) => (
           <NavLink to={singleSidebarData.url}>
             {({ isActive }) => (
-              <div className="w-full h-[60px] flex items-center gap-[21px]">
+              <div className="w-full h-[60px] max-2xl:h-[48px] flex items-center gap-[21px] max-2xl:gap-[16.8px]">
                 {isActive ? <ActiveSidebarBorder /> : <InactiveSidebarBorder />}
 
-                <div className="flex gap-[8px]">
-                  <img
+                <div className="flex gap-[8px] max-2xl:gap[6.4px] items-center">
+                  <Icon
+                    icon={singleSidebarData.iconName}
+                    className={`w-[24px] max-2xl:w-[19.2px] h-[24px] max-2xl:h-[19.2px] ${
+                      isActive ? "text-mainBlue" : "text-blackTextColor"
+                    }`}
+                  />
+                  {/* <img
                     src={
                       isActive
                         ? singleSidebarData.activeImage
                         : singleSidebarData.inActiveImage
                     }
                     alt="dashboardIcon"
-                  />
+                  /> */}
                   <div
-                    className={`text-[16px] font-[400] leading-[28px] ${
+                    className={`text-[16px] max-2xl:text-[12.8px] font-[400] leading-[28px] max-2xl:leading-[22.4px] ${
                       isActive ? "text-mainBlue" : "text-blackTextColor"
                     }`}
                   >
@@ -82,13 +97,17 @@ const Sidebar = () => {
             )}
           </NavLink>
         ))}
-        <div className="w-full h-[60px] flex items-center gap-[21px]">
+        <div className="w-full h-[60px] max-2xl:h-[48px] flex items-center gap-[21px] max-2xl:gap-[16.8px]">
           <InactiveSidebarBorder />
           <div className="flex w-full justify-between">
-            <div className="flex gap-[8px]">
-              <img src={activityLogInactive} alt="activityLogInactive" />
+            <div className="flex gap-[8px] max-2xl:gap[6.4px]">
+              <Icon
+                icon="icon-park-solid:activity-source"
+                className={`w-[24px] max-2xl:w-[19.2px] h-[24px] max-2xl:h-[19.2px]`}
+              />
+
               <div
-                className={`text-[16px] font-[400] leading-[28px] text-blackTextColor`}
+                className={`text-[16px] max-2xl:text-[12.8px] font-[400] leading-[28px] max-2xl:leading-[22.4px] text-blackTextColor`}
               >
                 Activity Log
               </div>
